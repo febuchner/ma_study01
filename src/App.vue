43ac819a03d06214736b2b8af5744a2a36770eb5
Felicitas Buchner <fe.buchner@t-online.de>
2023-01-07 00:11:43 +0100

    WIP(informed consent) Add InformedConsent component
    next clicking logic needs to be implemented

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import InformedConsent from './components/InformedConsent.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <img alt="labintheWild logo" class="logo d-inline-flex mb-5" src="@/assets/labintheWild-logo.png" width="150"/>
   </header>
+  <main role="main" class="container">
+    <InformedConsent/>
 
-  <RouterView />
+  </main>
+
+<!--  <RouterView />-->
 </template>
 
 <style scoped>
@@ -58,19 +53,11 @@ nav a:first-of-type {
 
 @media (min-width: 1024px) {
   header {
-    display: flex;
-    place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
 
   .logo {
     margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
 
   nav {
