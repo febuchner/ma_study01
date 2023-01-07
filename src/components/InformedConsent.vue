c7fcc0907bfa5904bcb5714ff1a5843f4ea6e4d0
Felicitas Buchner <fe.buchner@t-online.de>
2023-01-07 05:06:35 +0100

    WIP(informed consent) Modify InformedConsent component
    next clicking logic needs to be implemented

--- src/components/InformedConsent.vue
+++ src/components/InformedConsent.vue
@@ -1,50 +1,108 @@
 <template>
-  <p>
-    <span class=""><i> Please read the following information carefully before proceeding. </i></span>
-  </p>
-  <p><b> Why we are doing this research: </b> The goal of this project is to collect data on the general public's
-    impression of a profession based on a short resume in the English language.
-  </p>
-  <p><b> What you will have to do: </b> After filling out demographic information, we will provide X.
-    IOn total, you will be presented with X examples. For each example, you will decide which of the five
-    given professions the person to whom the resume belongs is most likely to have.
-  </p>
-  <p><b> What you will get out of it: </b> You will see X.
-  </p>
-  <p><b> Are there any risks: </b> We do not anticipate any risks. You can always choose to leave this study by closing the browser.
-  </p>
-  <p><b> Privacy and data collection: </b> Your responses will be handled privately. We do not keep any data that could be used to establish your identity directly.
-    We will store your demographic data securely in your browser only with your consent.
-    We do securely store your previous session identifiers on your browser.
-  </p>
-  <p><b> Expected duration: </b> X minutes
-  </p>
-  <p><b> Contact information: </b> This study is conducted at the Ludwig-Maximilians-University Munich in cooperation
-    with fortiss GmbH - Research Institute of the Free State of Bavaria for software-intensive systems.
-    If you have questions about this research, you may contact
-    Felicitas Buchner at Ludwig-Maximilians-University Munich by mailing at <a href="mailto:fe.buchner@campus.lmu.de">fe.buchner@campus.lmu.de</a>.
-  </p>
+<!--  <transition name="fade">-->
+    <div>
+<!--        :id="this.$options.name"-->
+<!--         v-if="this.$store.getters.currentStep == this.$options.name">-->
+      <p>
+        <span class=""><i> Please read the following information carefully before proceeding. </i></span>
+      </p>
+      <p><b> Why we are doing this research: </b> The goal of this project is to collect data on the general public's
+        impression of a profession based on a short resume in the English language.
+      </p>
+      <p><b> What you will have to do: </b> After filling out demographic information, we will provide X.
+        IOn total, you will be presented with X examples. For each example, you will decide which of the five
+        given professions the person to whom the resume belongs is most likely to have.
+      </p>
+      <p><b> What you will get out of it: </b> You will see X.
+      </p>
+      <p><b> Are there any risks: </b> We do not anticipate any risks. You can always choose to leave this study by
+        closing
+        the browser.
+      </p>
+      <p><b> Privacy and data collection: </b> Your responses will be handled privately. We do not keep any data that
+        could
+        be used to establish your identity directly.
+        We will store your demographic data securely in your browser only with your consent.
+        We do securely store your previous session identifiers on your browser.
+      </p>
+      <p><b> Expected duration: </b> X minutes
+      </p>
+      <p><b> Contact information: </b> This study is conducted at the Ludwig-Maximilians-University Munich in
+        cooperation
+        with fortiss GmbH - Research Institute of the Free State of Bavaria for software-intensive systems.
+        If you have questions about this research, you may contact
+        Felicitas Buchner at Ludwig-Maximilians-University Munich by mailing at <a
+            href="mailto:fe.buchner@campus.lmu.de">fe.buchner@campus.lmu.de</a>.
+      </p>
 
-  <hr class="my-4"/>
-  <label class="ant-checkbox-wrapper sc-clsFYl gLKwUQ"><span class="ant-checkbox ant-checkbox-checked"><input
-      type="checkbox" class="ant-checkbox-input" value="true"><span
-      class="ant-checkbox-inner"></span></span><span>I agree to participate in this research. Participation in this research is voluntary, and I can stop at any time without penalty. I feel that I understand what I am getting into, and I know I am free to leave the experiment at any time by simply closing the web browser.</span></label>
-  <button type="button" class="ant-btn ant-btn-primary ant-btn-lg sc-iBaQBe jzwDVq"
-          style="background-color: rgb(15, 169, 182); border-color: rgb(15, 169, 182);"><span>Next <span
-      role="img" aria-label="arrow-right" class="anticon anticon-arrow-right"><svg viewBox="64 64 896 896"
-                                                                                   focusable="false"
-                                                                                   data-icon="arrow-right"
-                                                                                   width="1em"
-                                                                                   height="1em"
-                                                                                   fill="currentColor"
-                                                                                   aria-hidden="true"><path
-      d="M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-.7 5.2-2L869 536.2a32.07 32.07 0 000-48.4z"></path></svg></span></span>
-  </button>
+      <hr class="my-4"/>
+
+      <div class="form-group">
+        <b-form-checkbox
+            id="consent-accepted"
+            v-model="consentAccepted"
+            name="consent-accepted"
+            :value="true"
+            :unchecked-value="false"
+            :state="stateConsentAccepted"
+        >I agree to participate in this research. Participation in this research is voluntary, and I can stop at any
+          time
+          without penalty. I feel that I understand what I am getting into, and I know I am free to leave the experiment
+          at
+          any time by simply closing the web browser.
+
+          <b-form-invalid-feedback :state="stateConsentAccepted">You must agree to the study participation in order to
+            participate in the study.
+          </b-form-invalid-feedback>
+
+        </b-form-checkbox>
+        <div>State: <strong>{{ state }}</strong></div>
+      </div>
+
+      <button @click="validateForm" type="submit" class="btn btn-primary">Next
+        <b-icon icon="arrow-right"></b-icon>
+      </button>
+
+      <button type="button" class="ant-btn ant-btn-primary ant-btn-lg sc-iBaQBe jzwDVq"
+              style="background-color: rgb(15, 169, 182); border-color: rgb(15, 169, 182);"><span>Next <span
+          role="img" aria-label="arrow-right" class="anticon anticon-arrow-right"><svg viewBox="64 64 896 896"
+                                                                                       focusable="false"
+                                                                                       data-icon="arrow-right"
+                                                                                       width="1em"
+                                                                                       height="1em"
+                                                                                       fill="currentColor"
+                                                                                       aria-hidden="true"><path
+          d="M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-.7 5.2-2L869 536.2a32.07 32.07 0 000-48.4z"></path></svg></span></span>
+      </button>
+    </div>
+<!--  </transition>-->
 </template>
 
 <script>
 export default {
-  name: "InformedConsent"
+  name: "InformedConsent",
+  computed: {
+    consentAccepted: {
+      // get() { return this.$store.state.answers['consent-accepted'] },
+      // set(value) { this.$store.commit('setAnswer', { 'key': 'consent-accepted', 'value': value }) }
+    },
+    stateConsentAccepted() {
+      return this.consentAccepted
+    }
+  },
+  methods: {
+    validateForm: function () {
+      // When consent is null force validation error
+      if (this.stateConsentAccepted == null) {
+        this.consentAccepted = false
+      }
+
+      if (this.stateConsentAccepted) {
+        // then go to next step
+        // this.$store.commit('nextStep')
+      }
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// import BootstrapVue from 'bootstrap-vue'
+
 
 import App from './App.vue'
 import router from './router'
 
+
 import './assets/main.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
 
 const app = createApp(App)
+
+// app.use(BootstrapVue)
 
 app.use(createPinia())
 app.use(router)
