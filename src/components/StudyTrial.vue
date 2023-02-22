948054ded0ae7bbccaf799b18a275937a1c70a3a
Felicitas Buchner <fe.buchner@t-online.de>
2023-02-22 02:26:28 +0100

    feat(legend) Add legend at highlight condition
    related:#32

--- src/components/StudyTrial.vue
+++ src/components/StudyTrial.vue
@@ -35,14 +35,34 @@
             {{ this.studyItems[this.trial_index]['profession'] }} <br>
             {{ this.studyItems[this.trial_index]['pred_profession'] }}
 
-            <div class="col-12 d-flex border-primary">
-              <div class="col d-flex justify-content-center">
+            <div class="col-12 row ms-0 d-flex border-primary">
+              <div class="col-12 d-flex justify-content-center">
                 <img src="../assets/images/my_robot.png" class="img-fluid robot">
                 <div class="h4 box sb2 ms-sm-3 my-3 my-md-5">My prediction is
                   <strong>{{ this.studyItems[this.trial_index]['pred_profession'] }}</strong>.
                 </div>
               </div>
+              <div v-if="this.studyCondition === 'with_explanation_highlights'" class="col-12 row mx-0 px-0">
+                <div class="col-12 col-md-4 h4 d-flex justify-content-center justify-content-md-end">
+
+                  not {{ this.studyItems[this.trial_index]['pred_profession'] }}
+                </div>
+                <div v-if="this.studyItems[this.trial_index]['pred_profession'] === 'professor'" class="off-set-3 col-6 offset-3 offset-sm-4 col-sm-4 offset-sm-4 offset-md-0 col-md-4 offset-md-0 bg-prof-0">
+                </div>
+                <div v-else-if="this.studyItems[this.trial_index]['pred_profession'] === 'physician'" class="off-set-3 col-6 offset-3 offset-sm-4 col-sm-4 offset-sm-4 offset-md-0 col-md-4 offset-md-0 bg-prof-1">
+                </div>
+                <div v-else-if="this.studyItems[this.trial_index]['pred_profession'] === 'psychologist'" class="off-set-3 col-6 offset-3 offset-sm-4 col-sm-4 offset-sm-4 offset-md-0 col-md-4 offset-md-0 bg-prof-2">
+                </div>
+                <div v-else-if="this.studyItems[this.trial_index]['pred_profession'] === 'teacher'" class="off-set-3 col-6 offset-3 offset-sm-4 col-sm-4 offset-sm-4 offset-md-0 col-md-4 offset-md-0 bg-prof-3">
+                </div>
+                <div v-else-if="this.studyItems[this.trial_index]['pred_profession'] === 'surgeon'" class="off-set-2 col-8 offset-2 offset-sm-4 col-sm-4 offset-sm-4 offset-md-0 col-md-4 offset-md-0 bg-prof-4">
+                </div>
+                <div class="col-12 col-md-4 h4 d-flex justify-content-center justify-content-md-start">
+                  {{ this.studyItems[this.trial_index]['pred_profession'] }}
+                </div>
+              </div>
             </div>
+
 
             <div class="col-12 bg-profession text-center mt-3 px-3 py-4">
               <div v-if="this.studyCondition === 'with_explanation_highlights'">
