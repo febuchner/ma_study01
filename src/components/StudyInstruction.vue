4008ae385bec6b1addc9c989769a93c8f728e8f0
Felicitas Buchner <fe.buchner@t-online.de>
2023-02-28 01:33:07 +0100

    task(profession explanation) Add profession explanation to instructions
    related:#32

--- src/components/StudyInstruction.vue
+++ src/components/StudyInstruction.vue
@@ -9,16 +9,42 @@
         </div>
 
         <div>
-          <p>During this part you will be shown 20 different sentences.
-            For each, again decide which of the five given professions the person to whom the resume belongs is most likely to have.
-            But this time, our AI will tell you their opinion about the profession, which you can consider or not.
-            <br><br>
-            <span>Based on your given answers from the first and second part, we will present you your gender bias compared to that of our AI at the end of the study.</span><br><br>
-          </p>
+          <div>During this part you will be shown 20 different sentences.
+            For each, again decide which of the five given professions the person to whom the resume belongs is most
+            likely to have.
+          </div>
+
+          <div class="mt-4">
+            These professions are:
+            <ul>
+              <li>professor - a teacher of the highest rank in a department of a British university, or a teacher of
+                high rank in an American university or college
+              </li>
+              <li>physician - a medical doctor, especially one who has general skill and is not a surgeon</li>
+              <li>psychologist - someone who studies the human mind and human emotions and behaviour, and how different
+                situations have an effect on people
+              </li>
+              <li>teacher - someone whose job is to teach in a school or college</li>
+              <li>surgeon - a doctor who is specially trained to perform medical operations</li>
+            </ul>
+          </div>
+
+          <div class="mt-4">
+            But this time, our AI tells you the profession it predicts for the person of the resume for you to consider.
+            <span v-if="this.store.study_condition === 'with_explanation_highlights'">
+            Also, the AI shows you highlighted words in the resume that led to its prediction.
+          </span>
+          </div>
+
+          <div class="mt-4">
+            Based on your given answers from the first and second part, we will present you your gender bias compared to
+            that of our AI at the end of the study.
+          </div>
         </div>
 
-        <div class="mb-3">
-          <p>Below you will find a picture that shows you what the study looks like.</p>
+        <div class="mt-4">
+          Below you will find a picture that shows you what the second part looks like with some instructions on how to
+          do it:
         </div>
 
         <img src="https://toxicity-litw.apps.allenai.org/93a19e44bf64e2fd43b0e6d31a6c25e3.png"
